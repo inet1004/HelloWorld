cb5d883e547b1158f6b665de0ee2135efae2d479
User <User@YD01-18>
2019-12-20 09:00:38 +0900

    xx

---
+++ HelloWorld/src/Operators/Exercise06.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class Exercise06 {
+	public static void main(String[] args) {
+		char star= '*';
+		for(int i=1;i<10;i++) {
+			for(int n=1;n<i;n++) {
+			System.out.print(star);
+			}
+			System.out.println("");
+		}
+		
+	}
+}
